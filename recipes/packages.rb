4e6e3d9a366813396d0524a2203565df97521e12
Levon Becker <levon.becker.github@bonusbits.com>
2017-04-05 13:35:14 -0700

    Fixed package update kernel excludes

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -7,14 +7,14 @@ suse_packages = node['bonusbits_base']['packages']['linux']['suse_packages']
 case node['platform']
 # Install Software Packages
 when 'debian', 'ubuntu'
-  execute 'apt-get update && apt-get -y upgrade exclude=kernel*'
+  execute 'apt-get update && apt-get -y upgrade --exclude=kernel*'
   include_recipe 'apt'
   package debian_packages
 when 'redhat', 'centos'
-  execute 'yum update -y exclude=kernel*'
+  execute 'yum update -y --exclude=kernel*'
   package redhat_packages
 when 'amazon'
-  execute 'yum update -y exclude=kernel*'
+  execute 'yum update -y --exclude=kernel*'
   package amazon_packages
 when 'suse', 'opensuse'
   package suse_packages
