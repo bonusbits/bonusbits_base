98e1e4c71eb9b3e8e1a0741ce10ce56f65102dfc
Levon Becker <levon.becker.github@bonusbits.com>
2017-04-10 00:27:53 -0700

    2.1.3 - Read Changelog

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'levon.becker.github@bonusbits.com'
 license 'MIT'
 description 'Foundation Wrapper Cookbook for all Nodes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.2'
+version '2.1.3'
 source_url 'https://github.com/bonusbits/bonusbits_base'
 issues_url 'https://github.com/bonusbits/bonusbits_base/issues'
 
